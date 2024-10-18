94859f45a271d41892d1f034a9cba982f3e55eeb
paras-14 <parasbhatia100@gmail.com>
2024-10-18 11:17:55 +0530

    Testing

--- SERVER/index.js
+++ SERVER/index.js
@@ -10,50 +10,54 @@ server.on('error', (err) => {
   server.close();
 });
 
-server.on('message', async (msg, rinfo) => {
-  try {
-    const incomingMessage = dnsPacket.decode(msg);
+// server.on('message', async (msg, rinfo) => {
+//   try {
+//     const incomingMessage = dnsPacket.decode(msg);
 
-    console.log(incomingMessage.questions[0]);
+//     console.log(incomingMessage.questions[0]);
 
-    const queryName = incomingMessage.questions[0].name;
+//     const queryName = incomingMessage.questions[0].name;
 
-    try {
-      const response = await axios.get('http://localhost:2000/dnsQuery', {
-        params: { hostname: queryName }
-      });
+//     try {
+//       const response = await axios.get('http://localhost:2000/dnsQuery', {
+//         params: { hostname: queryName }
+//       });
 
-      const ipFromDb = response.data;
+//       const ipFromDb = response.data;
 
-      // console.log("data",ipFromDb);
+//       // console.log("data",ipFromDb);
 
-      if (ipFromDb) {
-        const answer = dnsPacket.encode({
-          type: 'response',
-          id: incomingMessage.id,
-          flags: dnsPacket.AUTHORITATIVE_ANSWER,
-          questions: incomingMessage.questions,
-          answers: [{
-            type: ipFromDb.type,
-            class: 'IN',
-            name: queryName,
-            data: ipFromDb.data,
-          }]
-        });
+//       if (ipFromDb) {
+//         const answer = dnsPacket.encode({
+//           type: 'response',
+//           id: incomingMessage.id,
+//           flags: dnsPacket.AUTHORITATIVE_ANSWER,
+//           questions: incomingMessage.questions,
+//           answers: [{
+//             type: ipFromDb.type,
+//             class: 'IN',
+//             name: queryName,
+//             data: ipFromDb.data,
+//           }]
+//         });
 
-        server.send(answer, rinfo.port, rinfo.address);
-      } else {
-        console.log(`Domain not found: ${queryName}`);
-      }
+//         server.send(answer, rinfo.port, rinfo.address);
+//       } else {
+//         console.log(`Domain not found: ${queryName}`);
+//       }
 
-    } catch (error) {
-      console.error('Error fetching DNS record:', error);
-    }
+//     } catch (error) {
+//       console.error('Error fetching DNS record:', error);
+//     }
 
-  } catch (error) {
-    console.error('Error handling message:', error);
-  }
-});
+//   } catch (error) {
+//     console.error('Error handling message:', error);
+//   }
+// });
+
+server.on('message',(msg,rinfo)=>{
+    console.log('Incoming message',msg.toString())
+})
 
 server.on('listening', () => {
   const address = server.address();
